915024403064e831b451730bab3596878c5c3676
yangsen <709422214@qq.com>
2019-05-29 10:17:57 +0800

    ys

---
+++ apps/redis/test/redis_connect_test.exs
@@ -0,0 +1,16 @@
+defmodule Redis.Repo.Query do
+  require Logger
+  alias Redis.Repo.Search.Service, as: Service
+  use Redis.DataCase
+  use ExUnit.Case, async: true
+
+  test "connect" do
+    password = "123456"
+    redis_url = "redis://:#{password}@localhost:7002/1"
+    {:ok, conn} = Redix.start_link(redis_url, name: :redix)
+    w = Redix.command(conn, ["SET", "foo",1])
+    w = Redix.command(conn, ["GET", "foo"])
+    Logger.warn "w:#{inspect w}"
+  end
+
+end
